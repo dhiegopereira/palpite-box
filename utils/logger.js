f6146fc538173f165ddf3ef0ef426b8e214e2130
Diego Pereira <dhiego_ti@hotmail.com>
2021-06-05 23:22:57 -0300

    log directory change

--- utils/logger.js
+++ utils/logger.js
@@ -2,7 +2,7 @@ import log4js from 'log4js'
 
 export const logger = (value) => {
     log4js.configure({
-        appenders: { palpiteBox: { type: "file", filename: './logs/palpiteBox.log' } },
+        appenders: { palpiteBox: { type: "file", filename: 'palpiteBox.log' } },
         categories: { default: { appenders: ['palpiteBox'], level: 'trace' } }
     })
     return log4js.getLogger(`[${value}]`)
